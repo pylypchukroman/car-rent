1badb8d79a22ff84a5befaa12db15cd3c7fe6af0
pylypchukroman <romanpylypchuk@gmail.com>
2023-12-06 22:20:45 +0200

    added sass, mixins, fonts and vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import style from './app.module.scss';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {Home} from "./pages/Home";
 import {Layout} from "./components/Layout";
