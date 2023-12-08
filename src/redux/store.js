1f78a44160c4bb9dbcfe2b3dec65cd0fef65b788
pylypchukroman <romanpylypchuk@gmail.com>
2023-12-08 16:02:01 +0200

    added resist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,30 @@ import {configureStore} from "@reduxjs/toolkit";
 import {carsReducer} from "./carsSlice";
 import {filterReducer} from "./filter/filterSlice";
 import {favoritesReducer} from "./favorites/favoritesSlice";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, favoritesReducer)
 
 export const store = configureStore({
     reducer: {
         carsList: carsReducer,
         filter: filterReducer,
-        favorites: favoritesReducer
-    }
-})
+        favorites: persistedReducer
+    },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+    devTools: process.env.NODE_ENV !== 'production'
+});
+
+export const persistor = persistStore(store);
