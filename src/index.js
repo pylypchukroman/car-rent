1949befe8d96cf1bd8418883b641a89089b92b0f
pylypchukroman <romanpylypchuk@gmail.com>
2023-12-09 20:31:59 +0200

    some routs fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.module.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 import {persistor, store} from "./redux/store";
 import {PersistGate} from "redux-persist/integration/react";
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </PersistGate>
     </Provider>
 );
