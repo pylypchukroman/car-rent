d44a9d37b4fd720adb0d0971d03fd4539d29f524
pylypchukroman <romanpylypchuk@gmail.com>
2026-07-06 19:39:25 +1000

    build update

--- src/hooks/useOptionData.jsx
+++ src/hooks/useOptionData.jsx
@@ -5,7 +5,7 @@ import {extractCorrectPrice} from "../helpers/extractCorrectPrice";
 export const useOptionData = () => {
     const allCarsList = useSelector(selectCars);
     const brandOptions = [...new Set(allCarsList.map(car => car.make).sort((a, b) => a.localeCompare(b)))];
-    const priceOptions = [...new Set(allCarsList.map(car => extractCorrectPrice(car.rentalPrice)).sort((a, b) => a -b))];
+    const priceOptions = [...new Set(allCarsList.map(car => extractCorrectPrice(car.rentalPrice)).sort((a, b) => a - b))];
 
     return {brandOptions, priceOptions}
 };
